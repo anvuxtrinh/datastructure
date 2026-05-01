4bed103200c23c0b49191a9b4af476026b982814
Khanh <trinhquockhanh2012199@gmail.com>
2026-05-02 05:20:07 +0700

    feat: add cstr

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "cstr.h"
 #include "vector.h"
 
 typedef struct {
@@ -8,6 +9,7 @@ typedef struct {
 } VecNum;
 
 int main(int argc, char *argv[]) {
+    // Test vector
     VecNum vec;
     vector_init(&vec);
     for(int i = 0; i < 10; i++) {
@@ -19,5 +21,19 @@ int main(int argc, char *argv[]) {
     printf("\n");
     vector_clear(&vec);
     vector_free(&vec);
+
+    // Test s8
+    s8 str;
+    s8_create_by_cptr(&str, "Hello World");
+    printf("%s\n", str.data);
+    s8_free(&str);
+
+    char *cptr = "Hello Again";
+    s8 str2;
+    s8_create_by_cptr(&str2, cptr);
+    printf("%s\n", str2.data);
+    s8_append(&str2, "!!!");
+    printf("%s\n", str2.data);
+    s8_free(&str2);
     return 0;
 }
